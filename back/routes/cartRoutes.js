7801393723ac9deb13e7a7f8b4c69b67c400f4bb
Taylor Robertson <taylorrobertson@Taylors-MacBook-Pro.local>
2020-11-21 12:21:46 -0500

    Create scaffolding for routes and database
    The backend now has the server.js file scaffolded for the routes
    and for connecting to the database.

---
+++ back/routes/cartRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+const carts = [];
+
+// Add item to cart
+// POST request
+
+// Delete item from cart
+// DELETE request
+
+module.exports = router;
--- back/server.js
+++ back/server.js
@@ -1,3 +1,31 @@
 const express = require('express');
+const cartRoutes = require('./routes/cartRoutes');
 
 const app = express();
+
+// Bring in the Mongoose
+const mongoose = require('mongoose');
+
+// Allow variables with .env to be read
+require('dotenv').config();
+
+const uri = process.env.REACT_APP_DATABASE;
+
+// Connect to the database
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Successfully connected to ${uri}`);
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+
+// Body Parser Middleware
+app.use(express.json({ extended: false }));
+
+app.use('/cart', cartRoutes);
+
+app.listen(4000, () => {
+  console.log('The server is running at port 4000');
+})
